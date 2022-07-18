d8654a98f7824b95cffd4c0bb39aa735fcb1f0a9
Visaj Nirav Shah <visaj.shah@gmail.com>
2022-07-18 16:11:41 +0530

    Add JSON Database and Fetch from it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React from "react";
+import {useEffect, useState} from "react";
+
 function App() {
+
+  const [solution, setSolution] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+      .then(res => res.json())
+      .then(json => {
+        // Generate a random integer to pick solution
+        const randomSolution = json[Math.floor(Math.random() * json.length)]
+        setSolution(randomSolution)
+      })
+  }, [setSolution])
+
   return (
     <div className="App">
       <h1>Rosebud</h1>
+      {solution && <div>Solution is: {solution.title}</div>}
     </div>
   );
 }
