12dcf6e80dd258d0768e23b377273e0623df5040
Visaj Nirav Shah <visaj.shah@gmail.com>
2022-07-24 15:55:29 +0530

    Update modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,29 +1,49 @@
 import React from "react";
 
 export default function Modal({ isCorrect, turn, solution }) {
-    return (
-        <div className="modal">
-            {isCorrect && (
-                <div>
-                    <h1>You Win!</h1>
-                    <p className="solution">{solution.movie}</p>
-                    <p>Directed by: {solution.director}</p>
-                    if ({turn} === 1) {
+    if (turn === 1) {
+        return (
+            <div className="modal">
+                {isCorrect && (
+                    <div>
+                        <h1>You Win!</h1>
+                        <p className="solution">{solution.movie}</p>
+                        <p>Directed by: {solution.director}</p>
                         <p>You found the solution in {turn} guess :)</p>
-                    }
-                    else {
+                    </div>
+                )}
+                {!isCorrect && (
+                    <div>
+                        <h1>Out of tries!</h1>
+                        <p className="solution">{solution.movie}</p>
+                        <p>Directed by: {solution.director}</p>
+                        <p>Better luck next time.</p>
+                    </div>
+                )}
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className="modal">
+                {isCorrect && (
+                    <div>
+                        <h1>You Win!</h1>
+                        <p className="solution">{solution.movie}</p>
+                        <p>Directed by: {solution.director}</p>
                         <p>You found the solution in {turn} guesses :)</p>
-                    }
-                </div>
-            )}
-            {!isCorrect && (
-                <div>
-                    <h1>Out of tries!</h1>
-                    <p className="solution">{solution.movie}</p>
-                    <p>Directed by: {solution.director}</p>
-                    <p>Better luck next time.</p>
-                </div>
-            )}
-        </div>
-    )
+                    </div>
+                )}
+                {!isCorrect && (
+                    <div>
+                        <h1>Out of tries!</h1>
+                        <p className="solution">{solution.movie}</p>
+                        <p>Directed by: {solution.director}</p>
+                        <p>Better luck next time.</p>
+                    </div>
+                )}
+            </div>
+        )
+    }
+    
 }
